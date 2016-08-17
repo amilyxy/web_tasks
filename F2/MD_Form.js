81c81153c3f0b03db9f8378c66ad1dd0f9611fab
amilyxy <amilyxy@163.com>
2016-08-17 23:43:26 +0800

    2改

--- F2/MD_Form.js
+++ F2/MD_Form.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
+  //-------------------------------------focus判断
+  $("input:text").focus(function(){
+      $("form label:eq(0)").addClass("label_up")
+      $("#email-val").addClass("text_bcg")
+  })
+  $("input:password").focus(function(){
+      $("form label:eq(1)").addClass("label_up")
+      $("#psw-val").addClass("password_bcg")
+  })
+  $("input:text").blur(function(){
+    var val = document.getElementById("email-val").value
+    if(val == null || val == "")
+      {
+         $("form label:eq(0)").removeClass("label_up")
+         $("#email-val").removeClass("text_bcg")
+       }
+  })
+  $("input:password").blur(function(){
+    var val = document.getElementById("psw-val").value
+    if(val == null || val == "")
+     {
+       $("form label:eq(1)").removeClass("label_up")
+       $("#psw-val").removeClass("password_bcg")
+      }
+  })
+//--------------------------------------Login判断
   $("#Login").click(function() {
     var check = validate_right()
     if(check)
@@ -9,6 +35,7 @@ $(document).ready(function() {
     }
   })
 })
+
 function validate_empty() {
   var empty_email = "please enter your email address "
   var empty_psw = "please enter your password "
@@ -30,13 +57,18 @@ function validate_empty() {
     var alerttxt_wrong = "please enter right email address"
     var emailval = document.getElementById("email-val").value
     var not_empty = validate_empty()
+    var email = document.getElementById("email-val").value
+    var check = /^[\w_\-\.]{2,20}@[\w]{2,10}\.[a-z]{2,3}(\.[a-z]{2})?/
+/* ^表示开头 \w=[a-zA-Z0-9]  g全局匹配(好像用不着)*/
+/*要求： 用户名只能包含字母数字以及下划线短线+@xxx+.xxx(+.xx()(.cn))*/
+/*可以用/../ 也可以用RegExp(new) */
     if (not_empty) {
-      var i = emailval.indexOf("@")
-      var j = emailval.lastIndexOf(".")
-      if (i < 1 || j - i < 2) {
+      if(!check.test(email)){
         alert(alerttxt_wrong)
         return false
-      } else
+          }
+      else {
         return true
+      }
     }
   }
